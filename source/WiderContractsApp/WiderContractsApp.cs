7685e5372f4b33d939d8940bed8de633d47a3a98
Jeremie Rossignol <jrossignol@gmail.com>
2016-02-28 13:44:28 -0700

    Fixed more issues with KSP 1.1 support.

--- source/WiderContractsApp/WiderContractsApp.cs
+++ source/WiderContractsApp/WiderContractsApp.cs
@@ -7,20 +7,25 @@ using UnityEngine;
 using UnityEngine.UI;
 using KSP;
 using KSP.UI;
+using KSP.UI.Screens;
 
 namespace WiderContractsApp
 {
     [KSPAddon(KSPAddon.Startup.EveryScene, false)]
     public class WiderContractsApp : MonoBehaviour
     {
-        private static GenericAppFrame contractsFrame = null;
-        private static GenericAppFrame engineerFrame = null;
+        private GenericAppFrame contractsFrame = null;
+        private GenericAppFrame engineerFrame = null;
 
         // Reflection fields we operate on
         static IEnumerable<FieldInfo> intFields = typeof(GenericAppFrame).GetFields(BindingFlags.NonPublic | BindingFlags.Instance).Where(mi => mi.FieldType == typeof(int));
         static FieldInfo widthField = intFields.First();
         static FieldInfo heightField = intFields.ElementAt(1);
         static FieldInfo transformField = typeof(GenericAppFrame).GetFields(BindingFlags.NonPublic | BindingFlags.Instance).Where(mi => mi.FieldType == typeof(RectTransform)).First();
+        static FieldInfo uiListPrefabField = typeof(ContractsApp).GetFields(BindingFlags.NonPublic | BindingFlags.Instance).Where(mi => mi.FieldType == typeof(UIListItem)).First();
+        static IEnumerable<FieldInfo> cascadingListFields = typeof(ContractsApp).GetFields(BindingFlags.NonPublic | BindingFlags.Instance).Where(mi => mi.FieldType == typeof(GenericCascadingList));
+        static FieldInfo contractsField = typeof(ContractsApp).GetFields(BindingFlags.NonPublic | BindingFlags.Instance).Where(mi => mi.FieldType == typeof(Dictionary<Guid, UICascadingList.CascadingListItem>)).First();
+        static MethodInfo refreshMethod = typeof(ContractsApp).GetMethods(BindingFlags.NonPublic | BindingFlags.Instance).Where(mi => mi.Name == "CreateContractsList").First();
 
         const float RESIZE_FACTOR = 1.6f;
 
@@ -62,7 +67,7 @@ namespace WiderContractsApp
         {
             // Try to find the app frame for the contracts window.  Note that we may pick up
             // the ones from the Engineer's report in the VAB/SPH instead, so check by name.
-            if (contractsFrame == null)
+            if (!contractsFrame)
             {
                 // Check if this scene even has a contracts app
                 IEnumerable<GenericAppFrame> frames = Resources.FindObjectsOfTypeAll<GenericAppFrame>();
@@ -85,10 +90,10 @@ namespace WiderContractsApp
                 }
             }
 
-            if (contractsFrame != null)
+            if (contractsFrame)
             {
-                Debug.Log("WiderContractsApp: Making adjustments to contract frame!!");
-
+                Debug.Log("WiderContractsApp: Making adjustments to contract frame!");
+                
                 // Set the new width and height (old value * factor)
                 int width = (int)(166 * RESIZE_FACTOR);
                 int height = (int)(176 * 2.5);
@@ -104,14 +109,57 @@ namespace WiderContractsApp
                 // Apply the changes
                 contractsFrame.Reposition();
 
-                // No need to hang around, the changes will stick for the lifetime of the app
-                Destroy(this);
+                if (ContractsApp.Instance != null)
+                {
+                    // Find the cascading list field
+                    foreach (FieldInfo cascadingListField in cascadingListFields)
+                    {
+                        GenericCascadingList ccl = (GenericCascadingList)cascadingListField.GetValue(ContractsApp.Instance);
+                        if (ccl != null)
+                        {
+                            // Set the body width (I think this is used for word wrap logic)
+                            ccl.bodyTextStartWidth = (int)(166 * RESIZE_FACTOR - (166 - 151));
+
+                            // Fix the prefab for the header - will apply to any newly added contracts
+                            FixListItem(ccl.cascadeHeader);
+
+                            // Fix the prefab for the body - used for contract notes
+                            FixListItem(ccl.cascadeBody);
+                        }
+                    }
+
+                    // Fix the prefab used for contract parameters
+                    UIListItem uiListPrefab = (UIListItem)uiListPrefabField.GetValue(ContractsApp.Instance);
+                    Text text = uiListPrefab.GetComponentsInChildren<Text>(true).FirstOrDefault();
+                    if (text)
+                    {
+                        text.rectTransform.sizeDelta = new Vector2(166 * RESIZE_FACTOR - (166 - 163), 14.0f);
+                    }
+
+                    // Refresh the contract list, forces everything to get recreated
+                    refreshMethod.Invoke(ContractsApp.Instance, new object[]{});
+
+                    // No need to hang around, the changes will stick for the lifetime of the app
+                    Destroy(this);
+                }
             }
 
             // In the past we needed to do some stuff to prevent leakage to the Engineer's frame, but that's no longer needed.
             // Keeping this around in case we need to do something with the engineer report in the future.
-            if (engineerFrame != null)
+            if (engineerFrame)
             {
+            }
+        }
+
+        private void FixListItem(UIListItem item)
+        {
+            float width = 166 * RESIZE_FACTOR - (166 - 163);
+
+            // Resize the item
+            Text t = item.GetComponentsInChildren<Text>(true).FirstOrDefault();
+            if (t)
+            {
+                t.rectTransform.sizeDelta = new Vector2(width, t.rectTransform.sizeDelta.y);
             }
         }
     }
